1f90ffe230927d4d0c03d6be74694c720113609b
SoTeKie <33070089+SoTeKie@users.noreply.github.com>
2020-06-17 01:48:24 +0200

    Created Store with it's required functions, might need a cleaner rewrite

---
+++ src/Store.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react'
+import list_context from './list_context'
+
+export default function Store({children}){
+	const initialState = {
+		items: [],
+		incItem: incItem,
+		decItem: decItem,
+		removeItem: removeItem
+	}
+
+	const [cart, setCart] = useState(initialState)
+
+	return(
+		<list_context.Provider value={cart}>
+			{children}
+		</list_context.Provider>
+	)
+
+	function incItem(incrementedItem){
+		let newItems = cart.items
+
+		const newItem = {
+			id:incrementedItem.id,
+			name:incrementedItem.name,
+			amount:1
+        }
+		
+		const filtered = newItems.filter(item => item.id === incrementedItem.id)
+
+		if (filtered.length > 0){
+			const index = newItems.map(item => item.id ).indexOf(incrementedItem.id)
+			newItems[index].amount += 1
+		}
+		else {
+			newItems.push(newItem)
+		}
+
+		setCart({...cart, items:newItems})
+
+	}
+
+	function decItem(decrementedItem){
+		const newItems = cart.items
+
+		const filtered = newItems.filter( item => item.id === decrementedItem.id)
+
+		if (filtered.length > 0){
+			const index = newItems.map(item => item.id ).indexOf(decrementedItem.id)
+			if (newItems[index].amount > 1){ 
+				newItems[index].amount -= 1 
+				setCart({...cart, items:newItems})
+			}
+			else {
+				cart.removeItem(decrementedItem)
+			}
+		}
+
+	}
+
+	function removeItem(removedItem){
+		const newItems = cart.items.filter( item => item.id !== removedItem.id)
+
+		setCart({...cart, items:newItems})
+	}
+}
