c66d0e7572e446f3f8a9a09d0310d43407d4969f
SoTeKie <33070089+SoTeKie@users.noreply.github.com>
2020-06-18 04:01:05 +0200

    Started reworking Orders.jsx

--- src/Orders.jsx
+++ src/Orders.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {appInstance} from './api_requests'
 
 export default function Orders (){
 
-	function handleClick() {
+	useEffect(() => {
 		appInstance.get('orders/')
 		.then( response => console.log(response))
 		.catch( error => console.log(error.response))
-	}
-
+	},[])
 	return(
 		<button onClick={handleClick}> Get orders! </button>
 	)
