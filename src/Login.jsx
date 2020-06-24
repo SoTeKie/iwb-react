89bd97f921faa14c10cdc962a40aa4e40d1f23ed
SoTeKie <33070089+SoTeKie@users.noreply.github.com>
2020-06-24 23:55:42 +0200

    Ugly styling

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
 import {authInstance} from './Helpers/api_requests'
 import history from './Helpers/history'
+import styles from './Stylesheets/Login.module.css'
 
 export default function Login(){
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+	const [loginError, setLoginError] = useState('')
 
     function handleNameChange (e) {
         setUsername(e.target.value)
@@ -15,6 +17,7 @@ export default function Login(){
     }
 
     function handleSubmit(e) {
+		setLoginError('')
         authInstance.post('', {
             username: username,
             password: password
@@ -26,26 +29,23 @@ export default function Login(){
 			GroupRedirect(response.data.user_group)
         })
         .catch( error => {
-            console.log(`ERROR: ${error}`)
+			setLoginError(`ERROR: ${error}`)
             localStorage.clear()
         })
         e.preventDefault()
     }
     
     return(
-        <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input type="text" value={username} onChange={handleNameChange} />
-            </label>
-            <br />
-            <label>
-                Password:
-                <input type="password" value={password} onChange={handlePasswordChange} />
-            </label>
-            
-            <button>Login!</button>
-        </form>
+		<form className={styles.container} onSubmit={handleSubmit}>
+			<label className={styles.formLabel}> Name: </label>
+			<input className={styles.input} type="text" value={username} onChange={handleNameChange} />
+
+			<label className={styles.formLabel}> Password: </label>
+			<input className={styles.input} type="password" value={password} onChange={handlePasswordChange} />
+			
+			<button className={styles.button}>Login!</button>
+			{loginError && <h2 className={styles.error}>An error has occured.</h2>}
+		</form>
     )
 }
 
--- src/TabComponents/StartOrder.jsx
+++ src/TabComponents/StartOrder.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import styles from '../Stylesheets/Store.module.css'
 
 export default function StartOrder() {
 	return (
-		<Link to='/make-order'>
-			<button>Start ordering! </button>
+		<Link className={styles.container} to='/make-order'>
+			<button className={styles.button}>Start ordering! </button>
 		</Link>
 	)
 }
