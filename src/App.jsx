7a6fe7d24172dc54bdeeb69bf5b67cf87839068d
SoTeKie <33070089+SoTeKie@users.noreply.github.com>
2020-07-08 21:48:31 +0200

    Edited routes to add item-view for bartenders

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import history from './Helpers/history'
 import Login, {GroupRedirect} from './Login'
 import PrivateRoute from './PrivateRoute'
 import Orders from './BarComponents/Orders'
+import Items from './BarComponents/Items'
 import StartOrder from './TabComponents/StartOrder'
 import MakeOrder from './TabComponents/MakeOrder'
 import Cart from './TabComponents/Cart'
@@ -18,9 +19,14 @@ export default function App() {
 				<Route exact path='/login'>
 					<Login />
 				</Route>
+				{/*Bartender routes*/}
 				<PrivateRoute exact path='/orders'>
 					<Orders />
 				</PrivateRoute>
+				<PrivateRoute exact path='/mark-items'>
+					<Items />
+				</PrivateRoute>
+				{/*Customer routes*/}
 				<PrivateRoute exact path='/start-order'>
 					<StartOrder />
 				</PrivateRoute>
@@ -40,6 +46,7 @@ export default function App() {
 				<Route exact path='/error'>
 					<Error />
 				</Route>
+				{/*Redirect*/}
 				<Route path='*'>
 					{() => GroupRedirect(localStorage.getItem('group'))}
 				</Route>
